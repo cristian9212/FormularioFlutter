1524512186e1a2ea13eb3b68567e31b4103d412e
JovenDan <danilojcantorm@gmail.com>
2025-03-19 20:06:08 -0500

    Formulario_Flutter

---
+++ formulario_flutter/lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Formulario en Flutter',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: FormularioScreen(),
+    );
+  }
+}
+
+class FormularioScreen extends StatefulWidget {
+  @override
+  _FormularioScreenState createState() => _FormularioScreenState();
+}
+
+class _FormularioScreenState extends State<FormularioScreen> {
+  final _formKey = GlobalKey<FormState>(); // Clave global del formulario
+  final TextEditingController _nombreController = TextEditingController();
+   final TextEditingController _telefonoController = TextEditingController();
+    final TextEditingController _direccionController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Formulario Flutter')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _nombreController,
+                decoration: InputDecoration(labelText: 'Nombre'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Ingrese su nombre';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _telefonoController,
+                decoration: InputDecoration(labelText: 'Telefono'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Ingrese su numero de telefono';
+                  }
+                  return null;
+                },
+              ),
+                SizedBox(height: 10),
+                TextFormField(
+                controller: _direccionController,
+                decoration: InputDecoration(labelText: 'Dirreción'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                 if (value == null || value.isEmpty) {
+                    return 'Ingrese su Dirreción';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(labelText: 'Contraseña'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.length < 6) {
+                    return 'La contraseña debe tener al menos 6 caracteres';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Formulario válido')),
+                    );
+                  }
+                },
+                child: Text('Enviar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
